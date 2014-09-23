d179b6b180ae46526919bf24c6170cd999d243ed
Julian Boilen <julianboilen@gmail.com>
2014-09-23 10:24:39 -0400

    Hash cookie to prevent dups

--- input.rb
+++ input.rb
@@ -46,4 +46,7 @@ class CookieInput < Input
 	def to_s
 		"Cookie #{key} from #{uri} with value #{initial_value}"
 	end
+  def hash
+    {:type => self.class.name, :uri => uri, :key => key, :initial_value => initial_value}.hash
+  end
 end

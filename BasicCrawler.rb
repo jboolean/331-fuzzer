ffeabc4d7fef7ad4f2ce2ea841eba4dcbed5ba8d
Justin Cotner <jdc9622@rit.edu>
2014-10-16 00:04:32 -0400

    Pushing changed to BasicCrawler so I can pull

--- BasicCrawler.rb
+++ BasicCrawler.rb
@@ -7,6 +7,7 @@ require 'net/http'
 agent = Mechanize.new
 website = 'http://127.0.0.1/dvwa/' #ARGV[0]
 vectors = File.readlines('vectors.txt')
+possibleVulnerabilities = Set.new
 
 page = agent.get(website)
 
@@ -19,7 +20,13 @@ page = agent.submit(form, form.buttons.first)
 
 #agent.get(website)
 
-pp page
+cookies = agent.cookies
+
+cookies.each do |cookie|
+  if cookie.name == 'security'
+    cookie.value = 'low'
+  end
+end
 
 page = agent.get(website + "vulnerabilities/sqli/")
 
@@ -28,12 +35,32 @@ page = agent.get(website + "vulnerabilities/sqli/")
 forms = page.forms
 
 forms.each do |form|
-  vectors.each do |vector|
-    form.fields.each do |field|
-      field.value = vector
-    end
+
+  pp form.method
+
+  form.fields.each do |field|
+    vector = "' OR '1'='1"
+    field.value = vector
 
     page = form.submit
-    pp page
+
+
+
+    if page.content.to_s.include? vector
+      possibleVulnerabilities << ('Vulnerability found with ' + vector)
+    end
   end
 end
+
+pp possibleVulnerabilities
+
+#forms.each do |form|
+#  vectors.each do |vector|
+#    form.fields.each do |field|
+#      field.value = vector
+#    end
+
+#    page = form.submit
+#    pp page
+#  end
+#end
